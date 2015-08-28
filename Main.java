9921b5ef157aeb71b3dec80308331e9de808a0b7
homealone944 <risingerjmr@gmail.com>
2015-08-28 13:41:57 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
+import java.swing.*;
+import java.awt.*;
 
+Public class Main extends JFrame
+{
+  private static final int Width = 800;
+  private static final int Height = 600;
+  
+  public theGame()
+  {
+    
+    
+  }
+  public static void main(String args[])
+  {
+    Main run = new Main();
+  }
+  
+}
